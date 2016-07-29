48302444d0aa30886020faac9c21f7250bd1d44d
htatuan <hoangthoanhtuan@gmail.com>
2016-07-29 22:17:30 +0700

    add project

---
+++ PluralSight/PluralSight/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(PluralSight.Startup))]
+namespace PluralSight
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
